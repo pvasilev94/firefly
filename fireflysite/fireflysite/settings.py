3e2d5f7dfd0704bafa21c071eb4a98344dd51481
Jiss James <jissjames360@gmail.com>
2018-02-27 20:25:25 +0000

    added reqts and modified settings

--- fireflysite/fireflysite/settings.py
+++ fireflysite/fireflysite/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'fireflysite.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    #
+    # local pgsql db
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'firefly',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
